8631eac69d38cf9c2eb390f4c73e5477f79be97b
Alexis <35.alexis.pele@gmail.com>
2021-08-18 17:49:07 +0200

    Updated API request messages

--- src/common/classes/AuracleApiResponse.ts
+++ src/common/classes/AuracleApiResponse.ts
@@ -3,11 +3,22 @@ import { Response } from "express"
 /**
  * An enum that contains all the default value for the ApiResponse class
  */
-enum AuracleApiMessages {
+enum AuracleApiResponseMessages {
     defaultValid = "La requête a été effectuée avec succès.",
-    defaultRedirect = "La requête redirige vers une autre URL.",
+    default201 = "La ressource a été créée avec succès.",
+
+    defaultAdditionnalAction = "La requête redirige vers une autre URL.",
+    default301 = "La ressource n'est plus accessible à cet URI et a été déplacée de manière permanente.",
+    default302 = "La ressource n'est plus accessible à cet URI et a été déplacée de manière temporaire.",
+
     defaultClientError = "Une erreur inconnue dûe à une erreur client s'est produite",
-    defaultServerError = "Une erreur serveur inconnue s'est produite, veuillez contactez l'administrateur de l'application."
+    default401 = "Vous n'êtes pas autorisé à consulter cette ressource car vous n'êtes pas identifié.",
+    default403 = "Vous n'êtes pas autorisé à consulter cette ressource car vous n'avez pas le niveau d'authentification requis.",
+    default404 = "La ressource demandée n'est pas ou plus disponible.",
+    default405 = "Cette méthode n'est pas disponible pour cette ressource.",
+    default409 = "La requête génère un conflit de ressource et ne peut aboutir.",
+
+    defaultServerError = "Une erreur serveur inconnue s'est produite, veuillez contactez l'administrateur de l'application.",
 }
 
 export class AuracleApiResponse {
@@ -26,22 +37,22 @@ export class AuracleApiResponse {
         else {
             // HTTP Valid ranges
             if ((this.status >= 200) && (this.status <= 299)) {
-                this.message = AuracleApiMessages.defaultValid
+                this.message = AuracleApiResponseMessages.defaultValid
             }
 
-            // HTTP Redirect ranges
+            // HTTP Additionnal Action ranges
             else if ((this.status >= 300) && (this.status <= 399)) {
-                this.message = AuracleApiMessages.defaultRedirect
+                this.message = AuracleApiResponseMessages.defaultAdditionnalAction
             }
 
             // HTTP Client Error ranges
             else if ((this.status >= 400) && (this.status <= 499)) {
-                this.message = AuracleApiMessages.defaultClientError
+                this.message = AuracleApiResponseMessages.defaultClientError
             }
 
             // HTTP Server Error ranges
             else if ((this.status >= 500) && (this.status <= 599)) {
-                this.message = AuracleApiMessages.defaultServerError
+                this.message = AuracleApiResponseMessages.defaultServerError
             }
         }
     }
